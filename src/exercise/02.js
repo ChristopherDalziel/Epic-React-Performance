1b3466ea3818f8cf17924f8c93e6bac32d43f56a
Christopher Dalziel <christopher.dalziel@lexicondigital.com.au>
2020-11-24 21:41:53 +1100

    02 complete

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,8 +3,11 @@
 
 import * as React from 'react'
 import { useCombobox } from '../use-combobox'
-import { getItems } from '../filter-cities'
+// import { getItems } from '../filter-cities'
 import { useForceRerender } from '../utils'
+// *extension2*
+import { getItems } from "../workerized-filter-cities"
+import { useAsync } from "../utils"
 
 function Menu({
   items,
@@ -60,8 +63,13 @@ function App() {
   const forceRerender = useForceRerender()
   const [inputValue, setInputValue] = React.useState('')
 
-  // 🐨 wrap getItems in a call to `React.useMemo`
-  const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+  // const allItems = React.useMemo(() => getItems(inputValue), [inputValue])
+
+  // *extension2*
+  const { data: allItems, run } = useAsync({ data: [], status: 'pending' })
+  React.useEffect(() => {
+    run(getItems(inputValue))
+  }, [inputValue, run])
   const items = allItems.slice(0, 100)
 
   const {
